8f4a1ab5418190bb628c6f5dfaacf6b14a86ccc3
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2022-05-13 05:54:02 -0300

    chore: tsconfig.json

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello Server"});
+});
 
 
-app.listen(3000, () => console.log("Server is running!"));
+app.listen(3000, () => console.log("Server is running!")); 
