ca2a28451333343173b10e4148159773911e0d83
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2022-05-13 06:05:25 -0300

    chore: debug

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello Server"});
 });
-
 
 app.listen(3000, () => console.log("Server is running!")); 
