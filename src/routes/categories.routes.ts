d06dc1725a523ad3e80e4aa7e3369134f732dff7
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2022-05-16 05:11:10 -0300

    feat: add services SOLID

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 
 const categoriesRoutes = Router();
@@ -8,13 +9,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if(categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category Already exists!"});
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  
+  createCategoryService.execute({ name, description });
   return response.status(201).send();
 });
 
@@ -23,6 +20,6 @@ categoriesRoutes.get("/", (request, response) => {
   return response.status(200).json(all);
 });
 
-export { categoriesRoutes }
+export { categoriesRoutes } 
 
  
